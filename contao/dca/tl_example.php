34df91fc9b0c0281f6068fd7546c785f6a1c1c23
Baltidus <ludovici.joffrey@gmail.com>
2023-10-01 21:38:23 +0200

    followed the setup from doc: starting up your developpement page. Add severals new folder to be able to create contents after

---
+++ contao/dca/tl_example.php
@@ -0,0 +1,10 @@
+<?php
+// contao/dca/tl_example.php
+/* !TODO - To determine ! 
+$GLOBALS['TL_DCA']['tl_example'] = [
+    'config' => […],
+    'list' => […],
+    'fields' => […],
+    'palettes' => […],
+];
+*/
